bccb2306046bf48bc1e6d954af278d0df28bbdae
Comandur Kannan <comandurkannan@hotmail.com>
2013-07-09 04:59:59 +0000

    Hello World from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs      = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf = new Buffer(256);
+  buf = fs.readFileSync('index.html')
+  // response.send('Hello World 2!');
+  console.log("$" + buf.toString() + "$");
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
